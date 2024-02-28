3c56946d200f7154839e00d22fc7cb8c4060fd38
username <wjdwntjd55@naver.com>
2024-02-28 20:02:02 +0900

    feat : 바텀네비게이션 구현

--- app/src/main/java/com/alltimeowl/payrit/ui/login/LoginActivity.kt
+++ app/src/main/java/com/alltimeowl/payrit/ui/login/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.alltimeowl.payrit.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alltimeowl.payrit.databinding.ActivityLoginBinding
+import com.alltimeowl.payrit.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,5 +15,15 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        moveToHome()
+    }
+
+    private fun moveToHome() {
+        binding.buttonNoLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
