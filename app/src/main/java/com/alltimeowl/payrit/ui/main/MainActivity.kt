c1f2d1c4f3f80d1715541a4fae6aa42b2717d5e1
username <wjdwntjd55@naver.com>
2024-03-01 13:30:14 +0900

    feat :  빌려준 기록, 빌린 기록의 기본 홈 화면 구현

--- app/src/main/java/com/alltimeowl/payrit/ui/main/MainActivity.kt
+++ app/src/main/java/com/alltimeowl/payrit/ui/main/MainActivity.kt
@@ -2,11 +2,80 @@ package com.alltimeowl.payrit.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.alltimeowl.payrit.R
+import com.alltimeowl.payrit.databinding.ActivityMainBinding
+import com.alltimeowl.payrit.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        replaceFragment(HOME_FRAGMENT, false, null)
+    }
+
+    fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle?){
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        var newFragment: Fragment? = null
+
+        newFragment = when(name){
+            HOME_FRAGMENT -> HomeFragment()
+
+            else -> Fragment()
+        }
+
+        newFragment?.arguments = bundle
+
+        if(newFragment != null) {
+
+            fragmentTransaction.replace(R.id.fragmentContainerView_main, newFragment)
+
+            if (addToBackStack == true) {
+                fragmentTransaction.addToBackStack(name)
+            }
+
+            fragmentTransaction.commit()
+        }
+    }
+
+    fun removeFragment(name: String){
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+
+    fun bottomNavigation() {
+
+        activityMainBinding.bottomNavigationViewMain.run {
+
+            setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.home_menu -> {
+                        replaceFragment(HOME_FRAGMENT, false, null)
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.write_menu -> {
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.my_menu -> {
+                        return@setOnItemSelectedListener true
+                    }
+
+                    else -> return@setOnItemSelectedListener false
+                }
+            }
+        }
+    }
+
+
+    companion object {
+
+        val HOME_FRAGMENT = "HomeFragment"
     }
 }
