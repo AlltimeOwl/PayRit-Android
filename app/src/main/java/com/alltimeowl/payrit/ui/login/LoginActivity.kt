b42e5614744d60005e90ff04d896bc63d4f9be16
username <wjdwntjd55@naver.com>
2024-03-08 21:23:34 +0900

    feat : 로그인 화면에서 뒤로 가기 두 번 했을 때 앱 종료되게 구현

--- app/src/main/java/com/alltimeowl/payrit/ui/login/LoginActivity.kt
+++ app/src/main/java/com/alltimeowl/payrit/ui/login/LoginActivity.kt
@@ -3,12 +3,16 @@ package com.alltimeowl.payrit.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.alltimeowl.payrit.databinding.ActivityLoginBinding
 import com.alltimeowl.payrit.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
+
+    private var backPressedTime: Long = 0
+    private val backPressedInterval = 2000 // 뒤로가기 버튼을 두 번 누르는 간격 (2초)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +27,20 @@ class LoginActivity : AppCompatActivity() {
         binding.buttonNoLogin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            // LoginActivity 종료 (스택 제거)
+            finish()
         }
 
+    }
+
+    // 뒤로가기 버튼 이벤트 처리
+    override fun onBackPressed() {
+        if (backPressedTime + backPressedInterval > System.currentTimeMillis()) {
+            super.onBackPressed()
+            finishAffinity() // 앱 종료
+        } else {
+            Toast.makeText(this, "뒤로 가기를 한 번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }
