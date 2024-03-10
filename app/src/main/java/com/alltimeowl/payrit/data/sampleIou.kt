8a0e2b3fab9f4315a82b1640905def2b3b7cb3fb
username <wjdwntjd55@naver.com>
2024-03-10 20:56:29 +0900

    design : 홈 화면 디자인 변경 - HomeAdapter 에 들어가는 item_iou.xml 디자인 변경

--- app/src/main/java/com/alltimeowl/payrit/data/sampleIou.kt
+++ app/src/main/java/com/alltimeowl/payrit/data/sampleIou.kt
@@ -2,7 +2,9 @@ package com.alltimeowl.payrit.data
 
 data class sampleIou(
     val period: String,
+    val type: String,
     val amount: String,
+    val repay: String,
     val name: String,
     val day: String,
     val state: String
@@ -11,35 +13,45 @@ data class sampleIou(
 val sampleDataList = mutableListOf(
     sampleIou(
         period = "2024. 02.26 ~ 2024. 03.26",
-        amount = "30,000원",
+        type = "빌려준 돈",
+        amount = "30,000",
+        repay = "0",
         name = "홍길동",
         day = "D-29",
         state = "승인 대기중"
     ),
     sampleIou(
         period = "2024. 03.01 ~ 2024. 03.10",
-        amount = "10,000원",
+        type = "빌린 돈",
+        amount = "10,000",
+        repay = "0",
         name = "이순신",
         day = "D-39",
         state = "결제 대기중"
     ),
     sampleIou(
         period = "2024. 03.10 ~ 2024. 03.19",
-        amount = "30,000원",
+        type = "빌려준 돈",
+        amount = "30,000",
+        repay = "10,000",
         name = "세종대왕",
         day = "D-59",
         state = "차용증 작성 완료"
     ),
     sampleIou(
         period = "2024. 04.01 ~ 2024. 06.10",
-        amount = "100,000원",
+        type = "빌린 돈",
+        amount = "100,000",
+        repay = "100,000",
         name = "장보고",
         day = "D-79",
         state = "기간 만료"
     ),
     sampleIou(
         period = "2024. 04.28 ~ 2025. 05.10",
-        amount = "10,000,000원",
+        type = "빌려준 돈",
+        amount = "10,000,000",
+        repay = "6,000,000",
         name = "정주성",
         day = "D-129",
         state = "차용증 작성 완료"
