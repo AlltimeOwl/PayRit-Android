b233e51939eba935070f9927681839f01e4c72fb
username <wjdwntjd55@naver.com>
2024-03-17 03:44:21 +0900

    design : 마이페이지 메인 화면 디자인 변경

--- app/src/main/java/com/alltimeowl/payrit/ui/mypage/MyPageMainFragment.kt
+++ app/src/main/java/com/alltimeowl/payrit/ui/mypage/MyPageMainFragment.kt
@@ -1,5 +1,6 @@
 package com.alltimeowl.payrit.ui.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.alltimeowl.payrit.databinding.FragmentMyPageMainBinding
 import com.alltimeowl.payrit.databinding.ItemUserLogoutBinding
+import com.alltimeowl.payrit.ui.login.LoginActivity
 import com.alltimeowl.payrit.ui.main.MainActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -56,6 +58,11 @@ class MyPageMainFragment : Fragment() {
 
             // 로그아웃 - 네
             itemUserLogoutBinding.textViewYesLogout.setOnClickListener {
+                mainActivity.removeAllBackStack()
+
+                val intent = Intent(mainActivity, LoginActivity::class.java)
+                mainActivity.startActivity(intent)
+
                 dialog.dismiss()
             }
 
