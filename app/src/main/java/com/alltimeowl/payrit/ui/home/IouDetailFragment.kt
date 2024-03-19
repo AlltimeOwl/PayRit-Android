08df8f8834ebef1b401f293a562e9df71c34da82
username <wjdwntjd55@naver.com>
2024-03-19 17:27:54 +0900

    design : 페이릿 상세 페이지 디자인 변경

--- app/src/main/java/com/alltimeowl/payrit/ui/home/IouDetailFragment.kt
+++ app/src/main/java/com/alltimeowl/payrit/ui/home/IouDetailFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.alltimeowl.payrit.R
 import com.alltimeowl.payrit.databinding.FragmentIouDetailBinding
 import com.alltimeowl.payrit.databinding.ItemCancelBinding
 import com.alltimeowl.payrit.databinding.ItemDocumentBinding
 import com.alltimeowl.payrit.ui.main.MainActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class IouDetailFragment : Fragment() {
@@ -41,19 +44,14 @@ class IouDetailFragment : Fragment() {
                 }
             }
 
-            recyclerViewIouDetail.run {
-                recyclerViewIouDetail.layoutManager = LinearLayoutManager(context)
-                adapter = IouDetailAdapter()
-            }
-
-            // 금액 입력하기 클릭
+            // 일부 상환 기록 클릭
             buttonRecordIouDetail.setOnClickListener {
                 mainActivity.replaceFragment(MainActivity.IOU_DETAIL_AMOUNT_RECEIVED_FRAGMENT, true, null)
             }
 
-            // 확대 클릭
-            imageViewEnlargeIouDetail.setOnClickListener {
-                showAlertDialog()
+            // PDF·메일 내보내기 클릭
+            buttonDocumentIouDetail.setOnClickListener {
+                showBottomSheet()
             }
 
             // 개인 메모 클릭
@@ -64,13 +62,36 @@ class IouDetailFragment : Fragment() {
         }
     }
 
-    private fun showAlertDialog() {
-        val itemDocumentBinding = ItemDocumentBinding.inflate(layoutInflater)
-        val builder = MaterialAlertDialogBuilder(mainActivity)
-        builder.setView(itemDocumentBinding.root)
-        val dialog = builder.create()
+    private fun showBottomSheet() {
+        val bottomSheetView = ItemDocumentBinding.inflate(layoutInflater)
+        val bottomSheetDialog = BottomSheetDialog(mainActivity)
 
-        dialog.show()
+        bottomSheetDialog.setOnShowListener {
+            val bottomSheet = bottomSheetDialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheet?.background = null
+
+            val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet!!)
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+            // PDF 다운
+            bottomSheetView.linearLayoutDownloadPdfItemDocument.setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+
+            // 이메일 전송
+            bottomSheetView.linearLayoutSendEmailItemDocument.setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+
+            // 취소
+            bottomSheetView.linearLayoutCancelItemDocument.setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+
+        }
+
+        bottomSheetDialog.setContentView(bottomSheetView.root)
+        bottomSheetDialog.show()
     }
 
 }
