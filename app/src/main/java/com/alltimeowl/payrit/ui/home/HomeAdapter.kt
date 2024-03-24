9cef55c00bd28d9876d343a0c9374406d00f808d
username <wjdwntjd55@naver.com>
2024-03-24 13:20:42 +0900

    feat : 돈 빌려준 사람이 보는 페이릿 상세 페이지를 서버와 연결

--- app/src/main/java/com/alltimeowl/payrit/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/alltimeowl/payrit/ui/home/HomeAdapter.kt
@@ -27,7 +27,11 @@ class HomeAdapter(val mainActivity: MainActivity, var myIouList: MutableList<get
                     Log.d("HomeFragment", "클릭된 iou : ${iou}")
 
                     if (iou.paperRole == "CREDITOR" && iou.paperStatus == "COMPLETE_WRITING") {
-                        mainActivity.replaceFragment(MainActivity.IOU_DETAIL_FRAGMENT, true, null)
+
+                        val bundle = Bundle()
+                        bundle.putInt("paperId", iou.paperId)
+
+                        mainActivity.replaceFragment(MainActivity.IOU_DETAIL_FRAGMENT, true, bundle)
                     } else if (iou.paperRole == "DEBTOR" && iou.paperStatus == "COMPLETE_WRITING") {
                         mainActivity.replaceFragment(MainActivity.IOU_BORROW_DETAIL_FRAGMENT, true, null)
                     } else if (iou.isWriter && iou.paperStatus == "WAITING_AGREE") {
