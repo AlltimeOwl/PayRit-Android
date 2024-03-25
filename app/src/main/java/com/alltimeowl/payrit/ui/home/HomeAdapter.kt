c13322a5e2d8bb51f0597659ebe248ce36aa63ca
username <wjdwntjd55@naver.com>
2024-03-25 21:39:20 +0900

    feat : 홈 화면에에서 스피너 클릭시 차용증이 최근 거래순, 오래된 순, 기간 만료 순 으로 보이도록 구현함

--- app/src/main/java/com/alltimeowl/payrit/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/alltimeowl/payrit/ui/home/HomeAdapter.kt
@@ -26,13 +26,13 @@ class HomeAdapter(val mainActivity: MainActivity, var myIouList: MutableList<get
 
                     Log.d("HomeFragment", "클릭된 iou : ${iou}")
 
-                    if (iou.paperRole == "CREDITOR" && iou.paperStatus == "COMPLETE_WRITING") {
+                    if (iou.paperRole == "CREDITOR" && (iou.paperStatus == "COMPLETE_WRITING" || iou.paperStatus == "EXPIRED")) {
 
                         val bundle = Bundle()
                         bundle.putInt("paperId", iou.paperId)
 
                         mainActivity.replaceFragment(MainActivity.IOU_DETAIL_FRAGMENT, true, bundle)
-                    } else if (iou.paperRole == "DEBTOR" && iou.paperStatus == "COMPLETE_WRITING") {
+                    } else if (iou.paperRole == "DEBTOR" && (iou.paperStatus == "COMPLETE_WRITING" || iou.paperStatus == "EXPIRED")) {
                         // 채무자 가 보는 페이릿 상세화면
 
                         val bundle = Bundle()
